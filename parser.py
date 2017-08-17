0b175f8b29e63e1303e312acc4cea8c00f46f3a2
Ihor Melnyk <ihor@Ihors-MacBook-Pro.local>
2017-08-17 09:50:29 +0300

    Final version

--- parser.py
+++ parser.py
@@ -27,21 +27,6 @@ class Page(object):
         self.soap = soap
 
     @staticmethod
-    def _get_domain_from_url(url):
-        """
-        Fetches domain from url
-        :param url: str
-        :return: str
-        """
-        splitted_url = urlparse(url)
-        domain = splitted_url.netloc
-
-        if domain.startswith('www.'):
-            domain = domain[5:]
-
-        return domain
-
-    @staticmethod
     def _fetch_url_from_a(a):
         """
 
@@ -50,39 +35,61 @@ class Page(object):
         """
         return a.get('href')
 
+    def _find_links(self):
+        """
+        Method that returns all a tags on the page
+        :return: list of bs4.Element
+        """
+        return self.soap.find_all('a')
+
     def fetch_urls(self):
         """
         Method that returns list of urls
         :return: generator
         """
-        for a in self.soap.find_all('a'):
+        for a in self._find_links():
             yield self._fetch_url_from_a(a)
 
-    @staticmethod
-    def _get_ip_from_url(domain):
-        """
 
-        :param domain: str domain of the webpage
-        :return: str ip address
-        """
-        try:
-            return socket.gethostbyname(domain)
-        except socket.gaierror:
-            if LOGGING:
-                logging.error("Can't fetch ip from domain %s", domain)
+def get_url_host_ip(url):
+    """
+    :param url: str url
+    Returns triple (url, domain, ip) only if every part is present
+    :return generator
+    """
+    domain = _get_domain_from_url(url)
+    ip = _get_ip_from_url(domain)
 
-    def get_urls_hosts_ips(self):
-        """
-        Returns triple (url, domain, ip) only if every part is present
-        :return generator
-        """
-        for url in self.fetch_urls():
+    if domain and ip:
+        return url, domain, ip
 
-            domain = self._get_domain_from_url(url)
-            ip = self._get_ip_from_url(url)
 
-            if domain and ip:
-                yield (url, domain, ip)
+def _get_ip_from_url(domain):
+    """
+
+    :param domain: str domain of the webpage
+    :return: str ip address
+    """
+    try:
+        return socket.gethostbyname(domain)
+    except socket.gaierror:
+        if LOGGING:
+            logging.error("Can't fetch ip from domain %s", domain)
+
+
+def _get_domain_from_url(url):
+    """
+    Fetches domain from url
+    :param url: str
+    :return: str
+    """
+    splitted_url = urlparse(url)
+    domain = splitted_url.netloc
+
+    if domain.startswith('www.'):
+        domain = domain[5:]
+
+    return domain
 
 
 def parse(content):
@@ -126,15 +133,28 @@ def get_pages_from_urls(urls):
         yield parse(content)
 
 
+def list_of_links_from_urls(urls):
+    """
+    Core function of the program, returns list of links from urls
+    :param urls: list of str
+    :return generator
+    """
+    pages = get_pages_from_urls(urls)
+    for page in pages:
+        for url in page.fetch_urls():
+            yield url
+
+
 def main():
     """
     Main function of the program
     """
     urls = sys.argv[1:]
 
-    pages = get_pages_from_urls(urls)
-    for page in pages:
-        print page.get_urls_hosts_ips()
+    for url in list_of_links_from_urls(urls):
+        result = get_url_host_ip(url)
+        if result:
+            print result
 
 
 if __name__ == '__main__':
