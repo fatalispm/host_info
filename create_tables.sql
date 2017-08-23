9107dd76e5e4790ca2285b54aace9b61e74ff533
Ihor Melnyk <ihor@ihors-mbp.ddns.softservecom.com>
2017-08-23 12:54:10 +0300

    Fix sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,27 +1,48 @@
 CREATE DATABASE host;
 
-CREATE TABLE urls (id INT NOT NULL AUTO_INCREMENT,
-url VARCHAR(50), 
-creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-modification_time TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-,PRIMARY KEY(`id`), UNIQUE(`url`));
+CREATE TABLE urls (
+  id                INT NOT NULL AUTO_INCREMENT,
+  url               VARCHAR(50),
+  creation_time     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+  modification_time TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  ,
+  PRIMARY KEY (`id`),
+  UNIQUE (`url`)
+);
 
-CREATE TABLE link_info (id INT NOT NULL AUTO_INCREMENT,
-link VARCHAR(255), domain VARCHAR(100),
- counter INT DEFAULT 0, 
- PRIMARY KEY(`id`), UNIQUE (`link`));
+CREATE TABLE link_info (
+  id      INT NOT NULL AUTO_INCREMENT,
+  link    VARCHAR(255),
+  domain  VARCHAR(100),
+  counter INT          DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE (`link`)
+);
 
-CREATE TABLE ip_info(id INT NOT NULL AUTO_INCREMENT,
-ip VARCHAR(255), 
-counter INT DEFAULT 0, PRIMARY KEY(`id`), UNIQUE(`ip`));
- 
-CREATE TABLE link_ips (link_id INT, ip_id INT, url_id INT, 
-    PRIMARY KEY (`link_id`, `ip_id`, `url_id`),
-    CONSTRAINT FOREIGN KEY `link_fk` (`link_id`) REFERENCES `link_info`
-    (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FOREIGN KEY `ip_fk` (`ip_id`) REFERENCES `ip_info`
-    (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FOREIGN KEY `url_fk` (`url_id`) REFERENCES `urls`
-    (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE ip_info (
+  id      INT NOT NULL AUTO_INCREMENT,
+  ip      VARCHAR(255),
+  counter INT          DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE (`ip`)
+);
+
+CREATE TABLE link_ips (
+  link_id INT,
+  ip_id   INT,
+  url_id  INT,
+  PRIMARY KEY (`link_id`, `ip_id`, `url_id`),
+  CONSTRAINT FOREIGN KEY `link_fk` (`link_id`) REFERENCES `link_info`
+  (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT FOREIGN KEY `ip_fk` (`ip_id`) REFERENCES `ip_info`
+  (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT FOREIGN KEY `url_fk` (`url_id`) REFERENCES `urls`
+  (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
