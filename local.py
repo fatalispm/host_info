02f0d3869f9a645a3d2cea3d299cc9b1672b63dc
Ihor Melnyk <ihor@ihors-mbp.ddns.softservecom.com>
2017-08-30 12:10:14 +0300

    local.py

---
+++ local.py
@@ -0,0 +1,25 @@
+"""
+Store user settings here
+"""
+
+import os
+
+import configparser
+
+CONFROOT = os.environ.get('CONFROOT', '')
+
+db_conf_path = os.path.join(CONFROOT, 'db.cfg')
+
+parser = configparser.ConfigParser(
+    dict(user='user', password='password', host='localhost',
+         database='example'))
+
+parser.read(db_conf_path)
+
+host = parser.get('db', 'host')
+username = parser.get('db', 'username')
+password = parser.get('db', 'password')
+db = parser.get('db', 'database')
+
+settings = dict(host=host, user=username, database=db, password=password)
+
